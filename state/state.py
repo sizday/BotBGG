51dff3989d06ac31a053aea27c31de6a723cac84
Denis Sizov <dsizov@rpa-robin.ru>
2022-12-16 00:59:00 +0300

    Add state

--- state/state.py
+++ state/state.py
@@ -3,9 +3,11 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 class Prediction(StatesGroup):
     waiting_username = State()
+    waiting_size = State()
     predict_games = State()
 
 
 class Similar(StatesGroup):
+    waiting_size = State()
     waiting_game = State()
     predict_games = State()
