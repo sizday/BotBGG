c52ffbb11f2b8ee857a998b4c33c16c3dd1cdb55
Denis Sizov <dsizov@rpa-robin.ru>
2022-12-16 16:04:34 +0300

    edit text

--- users/user_panel.py
+++ users/user_panel.py
@@ -20,15 +20,15 @@ async def cancel(message: Message, state: FSMContext):
 
 @dp.message_handler(CommandStart())
 async def predict_user(message: Message):
-    await message.answer('В данном боте есть 2 функции:\n'
-                         '1. Предсказание новых настольных игр на основе данных с BGG - /predict\n'
-                         '2. Поиск похожих игр на определенную игру - /similar')
+    await message.answer('Привет! Я - бот-настольщик, который поможет тебе в выборе настольных игр:\n'
+                         '1. Подберу настольную игру на основе твоих предпочтений (с сайта BGG) - /predict\n'
+                         '2. Найду настольные игры, похожие на твою любимую настолку - /similar')
 
 
 @dp.message_handler(Command("predict"))
 async def predict_user(message: Message):
     bgg_link = hlink('BoardGamesGeek', 'https://boardgamegeek.com/')
-    await message.answer(f'Чтобы сделать предсказание, мне необходим твой логин с сайта {bgg_link}')
+    await message.answer(f'Чтобы помочь тебе, мне нужен твой логин с сайта {bgg_link}')
     await Prediction.waiting_username.set()
 
 
@@ -48,14 +48,15 @@ async def wait_username(message: Message, state: FSMContext):
             await message.answer(f"Спасибо за предоставленные данные. В твоем аккаунте {count_ratings} оценок.")
             await message.answer_sticker(positive_sticker.get(random.randint(0, len(positive_sticker)-1)))
             await message.answer(f"Выбери режим предсказания:\n"
-                                 f"1. Быстрее, но не идеально точно, так как обучение идет на небольшом объеме данных\n"
-                                 f"2. Точнее, но, к сожалению, медленнее",
+                                 f"1. Быстрое предсказание\n"
+                                 f"2. Точное предсказание (потребуется чуть больше времени)",
                                  reply_markup=data_size_menu)
             await state.update_data(username=username)
             await state.update_data(user_df=user_df)
             await Prediction.waiting_size.set()
         else:
-            await message.answer("К сожалению, в твоем аккаунте нет оценок и мы не можем сделать предсказание.")
+            await message.answer("К сожалению, в твоем аккаунте нет оценок, "
+                                 "и я не могу подобрать настольную игру для тебя.")
             await message.answer_sticker(negative_sticker.get(random.randint(0, len(negative_sticker)-1)))
             await state.reset_state()
 
@@ -71,16 +72,16 @@ async def get_predict_size(message: Message, state: FSMContext):
         data_size = DataSize.large
         time = '2-3 минуты'
     else:
-        await message.answer(f"Такой вариант не предусмотрен. Выбери из двух заданных.",
-                             reply_markup=ReplyKeyboardRemove())
+        await message.answer(f"Такой вариант не предусмотрен. Выбери из двух возможных.")
         await Prediction.waiting_size.set()
-    state_data = await state.get_data()
-    user_df = state_data.get("user_df")
-    data = get_overall_df(user_df, data_size)
-    await message.answer(f"Напиши сколько игр ты хочешь получить?", reply_markup=ReplyKeyboardRemove())
-    await state.update_data(user_data=data)
-    await state.update_data(predict_time=time)
-    await Prediction.predict_games.set()
+    if time:
+        state_data = await state.get_data()
+        user_df = state_data.get("user_df")
+        data = get_overall_df(user_df, data_size)
+        await message.answer(f"Напиши, сколько игр ты хочешь получить?", reply_markup=ReplyKeyboardRemove())
+        await state.update_data(user_data=data)
+        await state.update_data(predict_time=time)
+        await Prediction.predict_games.set()
 
 
 @dp.message_handler(state=Prediction.predict_games)
@@ -96,7 +97,7 @@ async def predict_games(message: Message, state: FSMContext):
             await message.answer(f"Дальнейший процесс займет некоторое время ({predict_time}). Никуда не уходи!")
             result_dict = predict(data, username, count, Method.recommend)
             result_str = create_str_from_dict(result_dict)
-            await message.answer(f"Ваши итоговые игры:\n{result_str}")
+            await message.answer(f"Игры, которые тебе могут понравится:\n{result_str}")
             await state.reset_state()
         else:
             await message.answer(f"Введеное значение должно быть больше нуля. Введите значение заново.")
@@ -109,8 +110,8 @@ async def predict_games(message: Message, state: FSMContext):
 @dp.message_handler(Command("similar"))
 async def cancel(message: Message):
     await message.answer(f"Для начала выбери режим поиска игр:\n"
-                         f"1. Быстрее, но не идеально точно, так как обучение идет на небольшом объеме данных\n"
-                         f"2. Точнее, но, к сожалению, медленнее", reply_markup=data_size_menu)
+                         f"1. Быстрое предсказание\n"
+                         f"2. Точное предсказание (потребуется чуть больше времени)", reply_markup=data_size_menu)
     await Similar.waiting_size.set()
 
 
@@ -123,18 +124,17 @@ async def get_similar_size(message: Message, state: FSMContext):
         time = '15-30 секунд'
     elif data_size == 'точнее':
         data_size = DataSize.large
-        time = '2-3 минуты'
+        time = '1-2 минуты'
     else:
-        await message.answer(f"Такой вариант не предусмотрен. Выбери из двух заданных.",
-                             reply_markup=ReplyKeyboardRemove())
+        await message.answer(f"Такой вариант не предусмотрен. Выбери из двух возможных.")
         await Similar.waiting_size.set()
-    await message.answer('Теперь напиши название игры, для которой ты хочешь найти схожие! '
-                         'Главное - необходимо писать точное название на английском языке',
-                         reply_markup=ReplyKeyboardRemove())
-    data = load_data_from_file(data_size)
-    await state.update_data(bgg_data=data)
-    await state.update_data(predict_time=time)
-    await Similar.waiting_game.set()
+    if time:
+        await message.answer('Напиши название настольной игры на английском, чтобы я подобрал похожие настолки',
+                             reply_markup=ReplyKeyboardRemove())
+        data = load_data_from_file(data_size)
+        await state.update_data(bgg_data=data)
+        await state.update_data(predict_time=time)
+        await Similar.waiting_game.set()
 
 
 @dp.message_handler(state=Similar.waiting_game)
@@ -144,12 +144,13 @@ async def wait_game(message: Message, state: FSMContext):
     data = state_data.get("bgg_data")
     game_id = get_game_id_by_name(data, game_name)
     if game_id is None:
-        await message.answer("К сожалению, я не смог найти такую игру. Попробуйте другую")
+        await message.answer("К сожалению, я не смог найти такую игру. "
+                             "Проверь правильность написания или введи другое название")
         await message.answer_sticker(negative_sticker.get(random.randint(0, len(negative_sticker) - 1)))
         await Similar.waiting_game.set()
     else:
         await message.answer_sticker(positive_sticker.get(random.randint(0, len(positive_sticker) - 1)))
-        await message.answer(f"Я нашел такую игру. Напиши сколько похожих игр ты хочешь получить?")
+        await message.answer(f"Я нашел такую игру. Напиши, сколько вариантов тебе предложить:")
         await state.update_data(game_id=game_id)
         await Similar.predict_games.set()
 
@@ -167,7 +168,7 @@ async def predict_similar(message: Message, state: FSMContext):
             await message.answer(f"Дальнейший процесс займет некоторое время ({predict_time}). Никуда не уходи!")
             result_dict = predict(data, game_id, count, Method.similar)
             result_str = create_str_from_dict(result_dict)
-            await message.answer(f"На заданную игру я нашел похожими вот такие игры:\n{result_str}")
+            await message.answer(f"Я нашел похожие игры: \n{result_str}")
             await state.reset_state()
         else:
             await message.answer(f"Введеное значение должно быть больше нуля. Введите значение заново.")
